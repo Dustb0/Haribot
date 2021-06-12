aec842bd5bd5641f43d61848de31b94eeee63d5a
Manuel <manuel.schmuki@gmail.com>
2021-06-12 08:28:12 +0200

    Scoped handlers

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from quiz import QuizHandler
 from quiz_spotlight import SpotlightQuizHandler
 
 client = discord.Client()
-quizHandler = None
+handlers = {}
 
 @client.event
 async def on_ready():
@@ -12,30 +12,43 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  global quizHandler
+  global handlers
 
   if message.author == client.user:
     return
 
-  # Quiz Handler
+  # Retrieve channel-scoped quiz handler
+  channelId = message.channel.id
+  quizHandler = handlers.get(channelId, None)
+
   if message.content.startswith('!q'):
     if quizHandler is None:
+      # Create quiz and add to scope
       quizHandler = QuizHandler(client)
+      handlers[channelId] = quizHandler
       await quizHandler.setup(message)
+
     else:
+      # End quiz and remove from scope
       await quizHandler.end_quiz(message)
+      handlers.pop(channelId)
       quizHandler = None
 
   if message.content.startswith('!sq'):
     if quizHandler is None:
+      # Create quiz and add to scope
       quizHandler = SpotlightQuizHandler(client)
+      handlers[channelId] = quizHandler
       await quizHandler.setup(message)
+
     else:
+      # End quiz and remove from scope
       await quizHandler.end_quiz(message)   
+      handlers.pop(channelId)
+      quizHandler = None
 
   elif quizHandler is not None and quizHandler.active():
     await quizHandler.handle_quiz(message)
-
 
 
 client.run(os.environ['TOKEN'])

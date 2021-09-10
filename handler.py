07e84063843e7748fbc01fc00f29a7a38ea736e1
Manuel <manuel.schmuki@gmail.com>
2021-09-10 12:01:44 +0200

    Start Refactor: Command Handling Channel Param

--- handler.py
+++ handler.py
@@ -1,6 +1,9 @@
 from jisho import JishoApi
 from commands.cmd_translate import process_translate_quiz
 
+# Each channel that's currently actively engaging with the bot gets its own
+# Handler instance. Its responsible for parsing input and directing it to 
+# the respective commands that contain the actual bot logic.
 class Handler:
     
     def __init__(self, client):
@@ -10,4 +13,5 @@ class Handler:
     def process_message(self, message):
         # Check command
         if message.content.startswith('!translateQuiz'):
-            process_translate_quiz(self.jishoApi, message)
+            channelName = message.content.replace('!translateQuiz', '').strip()
+            process_translate_quiz(self.jishoApi, message, channelName)

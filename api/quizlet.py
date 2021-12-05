3084576a98ac4d110a3f132cff9ee7697635d54f
Manuel <manuel.schmuki@gmail.com>
2021-12-05 16:55:54 +0100

    Update translation quiz to quizlet

--- api/quizlet.py
+++ api/quizlet.py
@@ -8,7 +8,11 @@ class QuizletApi:
 
   def get_vocabulary(self, url):
     list = []
-    req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    req = urllib.request.Request(url, headers={
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36',
+      'sec-ch-ua-platform': "Windows",
+      'sec-ch-ua-platform': "Windows"
+      })
 
     try:
       with urllib.request.urlopen(req) as response:
